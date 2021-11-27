48d1fe3ab0072f5f32a5d3c7bfabc4519a8ac6fb
DonnyGru <91632259+DonnyGru@users.noreply.github.com>
2021-11-27 18:41:55 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+console.log("Hello teacher!")
+console.log("Function basics: exercise")
+
+// Make a pizza
+// step 1 Put sauce on the pizza
+// step 2 Add cheese and other ingredients
+// step 3 Bake the pizza
+
+const makePizza = function(){
+    console.log("Put cheese on the pizza");
+  console.log("Add cheese and other ingredients");
+  console.log("Bake the pizza");
+}
+/*
+makePizza();
+makePizza();
+makePizza();
+*/
+
+// Make a sushi
+// step 1 Prepare the fish
+// step 2 Bake the rice
+// step 3 Roll everything in the seaweed
+// step 4 Slice the sushi
+
+const makeSushi = function(){
+    console.log("Prepare the fish");
+  console.log("Bake the rice");
+  console.log("Roll everything in the seaweed");
+  console.log("Slice the sushi");
+}
+
+//Three times sushi and two pizza orders
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
